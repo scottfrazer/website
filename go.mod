54848520134120b113ae95b1f7bc6b1cfd8e083b
Scott Frazer <scott.d.frazer@gmail.com>
2024-02-14 19:22:46 -0500

    testing db connections

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/scottfrazer/website
 
 go 1.21.7
 
-require github.com/go-chi/chi/v5 v5.0.11 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.11 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/lib/pq"
 )
 
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
+	db, err := sql.Open("postgres", os.Args[1])
+	check(err)
+
+	rows, err := db.Query("SELECT id, data FROM test_table")
+	check(err)
+	for rows.Next() {
+		var id int64
+		var data string
+		check(rows.Scan(&id, &data))
+		fmt.Printf("(%d, %s)\n", id, data)
+	}
+	rows.Close()
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
