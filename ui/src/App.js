f6c9befd9290d2442cdd0f1e40185bb195c90c3e
Scott Frazer <scott.d.frazer@gmail.com>
2024-02-17 07:51:36 -0500

    test

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
 
+  console.log(process.env)
   const baseUrl = process.env.API_BASE_URL || 'http://localhost:8080';
 
   const [start, setStart] = useState([]);
